3bbab99d458170a557e75bfd6c879378b0fe2d52
Vardaan <vardaan.agarwal.98@gmail.com>
2021-08-31 21:33:51 +0530

    create directories and i/o extraction and insertion

---
+++ DsAndAlgo/basic_IO.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "Simple Program to add two numbers\n\n";
+    cout << "enter 1st number\n";
+    cin >> num1;
+
+    cout << "enter second number\n";
+    cin >> num2;
+
+    cout << "The Sum Is " << num1 + num2;
+    return 0;
+}
