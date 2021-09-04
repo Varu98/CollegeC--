10e24154302bba5918b9d456cc9537b84706713c
Vardaan <vardaan.agarwal.98@gmail.com>
2021-09-04 18:24:15 +0530

    initial commit

---
+++ class2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class person
+{
+    int age;
+    char name[30];
+
+public:
+    void getData();
+    void display();
+};
+
+void person::getData()
+{
+    cout << "Enter Your Name ";
+    cin >> name;
+    cout << "Enter Your Age ";
+    cin >> age;
+}
+
+void person::display()
+{
+    cout << "\n Name is " << name << endl;
+    cout << "\n age is " << age << endl;
+}
+
+int main()
+{
+    person p;
+    p.getData();
+    p.display();
+
+    return 0;
+}
